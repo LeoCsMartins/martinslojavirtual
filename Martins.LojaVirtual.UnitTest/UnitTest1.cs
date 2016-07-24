aed073c9283d4c639f2979e7213f06d87d05173b
@LeoCsMartins <leonardocsmartins@gmail.com>
2016-07-24 15:45:49 -0300

    First Commit

---
+++ Martins.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Martins.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestMartins
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
